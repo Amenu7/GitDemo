11e9ce2754e3863a6edb661e6c1a428c70d71af0
Amenu7 <amanu@tutanota.com>
2020-03-21 08:54:14 +0300

    asdfsa

--- src/end.java
+++ src/end.java
@@ -23,17 +23,7 @@ public class end {
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 		driver.findElement(By.xpath("//a[@value='KQH']")).click();
 		
-		//developer first
-
-		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-		driver.findElement(By.xpath("//a[@value='KQH']")).click();
 		
-		//developer second
-
-		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-		driver.findElement(By.xpath("//a[@value='KQH']")).click();
-		
-		driver.findElement(By.xpath("//a[@value='KQH']")).click();
 	}
 	
 
